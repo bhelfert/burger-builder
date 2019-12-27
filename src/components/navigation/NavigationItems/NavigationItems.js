083f060651a035b012d0e018a68f3d02f48b4728
Bastian Helfert <bastian.helfert@gmail.com>
2019-12-27 20:19:37 +0100

    Make link to Orders only available when user is authenticated

--- src/components/navigation/NavigationItems/NavigationItems.js
+++ src/components/navigation/NavigationItems/NavigationItems.js
@@ -6,7 +6,7 @@ import styles from './NavigationItems.module.css';
 const NavigationItems = props =>
     <ul className={styles.navigationItems}>
         <NavigationItem link='/' exact>Burger Builder</NavigationItem>
-        <NavigationItem link='/orders'>Orders</NavigationItem>
+        {props.isAuthenticated && <NavigationItem link='/orders'>Orders</NavigationItem>}
         {!props.isAuthenticated
             ? <NavigationItem link='/auth'>Sign In or Up</NavigationItem>
             : <NavigationItem link='/logout'>Log Out</NavigationItem>}
